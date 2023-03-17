8f377e9a195e3cde00e8e733aed152500b48cfd2
Replit user <>
2023-03-17 04:00:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -57,7 +57,7 @@ Persona* crearPersona(char nombre[], char rut[], int edad) {
    Persona *p = (Persona *) malloc(sizeof(Persona));
    //asignar valores de entrada a los campos de p
   if(p == NULL) {
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
   strcpy(p->nombre, nombre);
   strcpy(p->rut, rut);
